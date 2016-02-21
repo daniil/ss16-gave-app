7ecc76e72afb2b863c31233b772b5f81e232f195
Aadhi Vive <aadhi.vive@gmail.com>
2016-02-21 13:25:26 -0500

    styling update - header styles

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styles from './home.scss';
+import classnames from 'classnames';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <p>Be a true BFF</p>
-        <p>Find the best gifts for 'em</p>
+      <div className={ classnames(styles.homeBannerText)}>
+        <p className={ classnames(styles.homeBannerTextp1)}>Be a true BFF</p>
+        <p className={ classnames(styles.homeBannerTextp2)}>Find the <strong>best gifts </strong> for 'em</p>
       </div>
     );
   }
