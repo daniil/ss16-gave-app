b6375cc1ddc3a44c0ada62a4e99fe64c0939996b
Daniil Molodkov <mr.me@daniil.ca>
2016-02-20 19:49:21 -0500

    Validation for wishlist search

--- src/components/ui/wishlist-search.js
+++ src/components/ui/wishlist-search.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styles from './styles.scss';
+import classnames from 'classnames';
 
 class WishlistSearch extends Component {
-
   state = {
     searchValue: '',
-    validated: true
+    validated: false
   };
+
+  componentDidMount() {
+    this.refs.searchByNumber.focus();
+  }
 
   render() {
     return (
         <div>
-          <form className="pure-form">
-            <input className="pure-input-1"
-                   type="text"
-                   ref="searchByNumber"
-                   placeholder="Search by phone number and press Enter"
-                   value={this.state.searchValue}
-                   onChange={::this.onChangeHandler}
-                   onKeyPress={::this.onKeyPressHandler} />
-
+          <form className="pure-form pure-form-stacked pure-g">
+            <div className="pure-u-22-24">
+              <label htmlFor="phoneNumber">Phone Number</label>
+              <input id="phoneNumber"
+                     className="pure-input-1"
+                     type="text"
+                     ref="searchByNumber"
+                     placeholder="Search by phone number"
+                     value={this.state.searchValue}
+                     onChange={::this.onChangeHandler}
+                     onKeyPress={::this.onKeyPressHandler} />
+            </div>
+            <div className={classnames(
+                   'pure-u-2-24',
+                   styles.searchStatusIcon
+                 )}>
+              <i className={classnames(
+                   'fa',
+                   styles.searchStatus,
+                   {['fa-eye-slash']: !this.state.validated},
+                   {[styles.searchStatusInvalid]: !this.state.validated},
+                   {['fa-check']: this.state.validated},
+                   {[styles.searchStatusValid]: this.state.validated}
+                 )}></i>
+            </div>
+            <div className="pure-u-1">
+              <span className={styles.instructions}>
+                Please enter 5 to 10 digits of the phone number only and press Enter when complete
+              </span>
+            </div>
           </form>
         </div>
     );
@@ -27,19 +53,24 @@ class WishlistSearch extends Component {
 
   onKeyPressHandler(e) {
     const { onWishlistSearch } = this.props;
+    const numberLength = this.state.searchValue.length;
 
     if (e && e.which === 13) {
-      onWishlistSearch(this.state.searchValue);
+      if (numberLength >= 5 && numberLength <= 10) {
+        onWishlistSearch(this.state.searchValue);
+      }
       e.preventDefault();
     }
   }
 
   onChangeHandler(e) {
+    const numberLength = e.target.value.length;
+
     this.setState({
+      validated: numberLength >= 5 && numberLength <= 10,
       searchValue: e.target.value.replace(/\D/g, '')
     });
   }
-
 }
 
 function mapStateToProps(state) {
