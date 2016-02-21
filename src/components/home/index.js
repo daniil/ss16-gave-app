fd3664459332fec95a885edf3358faa3ffbb1b21
Aadhi Vive <aadhi.vive@gmail.com>
2016-02-21 14:21:23 -0500

    styling update  - fixes for home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,12 +5,21 @@ import classnames from 'classnames';
 
 class Home extends Component {
   render() {
-    return (
-      <div className={ classnames(styles.homeBannerText)}>
-        <p className={ classnames(styles.homeBannerTextp1)}>Be a true BFF</p>
-        <p className={ classnames(styles.homeBannerTextp2)}>Find the <strong>best gifts </strong> for 'em</p>
-      </div>
-    );
+    let html;
+    const { app } = this.props;
+
+    if(app.isAuthenticated) {
+      html = (<p>Welcome back</p>)
+    } else {
+      html = (
+          <div className={ classnames(styles.homeBannerText)}>
+            <p className={ classnames(styles.homeBannerTextp1)}>Be a true BFF</p>
+            <p className={ classnames(styles.homeBannerTextp2)}>Find the <strong>best gifts </strong> for 'em</p>
+          </div>
+        );
+      }
+
+    return ( <div>{ html } </div> );
   }
 }
 
