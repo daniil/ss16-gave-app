a0a7ba59a454dfe44979a6dc955c6ced75a6cac2
Daniil Molodkov <mr.me@daniil.ca>
2016-02-21 14:44:52 -0500

    Moved login button to home page

--- src/components/nav/top-nav.js
+++ src/components/nav/top-nav.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { loginUser } from 'actions/app-actions';
 import WishlistSearch from 'components/ui/wishlist-search';
 import { routeActions } from 'react-router-redux';
 import baseStyles from 'styles/base.scss';
-import classnames from 'classnames';
 
 export default class TopNav extends Component {
   render() {
@@ -13,31 +11,11 @@ export default class TopNav extends Component {
       <div className={baseStyles.topNav}>
         <h1>GaveApp</h1>
         {
-          !app.isAuthenticated &&
-          <button className={
-                    classnames(
-                        'pure-button',
-                        'pure-button-primary',
-                        baseStyles.pureButton,
-                        baseStyles.pureButtonPrimary
-                    )
-                  }
-                  onClick={::this.onLogin}>
-            <i className="fa fa-facebook-square"></i>
-            Login with Facebook
-          </button>
-        }
-        {
           app.isAuthenticated &&
           <WishlistSearch onWishlistSearch={::this.onWishlistSearch} />
         }
       </div>
     );
-  }
-
-  onLogin() {
-    const { dispatch } = this.props;
-    dispatch(loginUser());
   }
 
   onWishlistSearch(val) {
